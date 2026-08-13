b78446166fdc5a44ed2057a03b947e371a34c0e3
Quyen <maiquyen030606@gmail.com>
2026-08-13 14:27:16 +0700

    J02020

---
+++ ARRAY/J02020/J02020.java
@@ -0,0 +1,31 @@
+package ARRAY.J02020;
+import java.util.*;
+public class J02020 {
+    static int n, k;
+    static int[] a;
+    static int dem=0;
+    public static void Try(int t){
+        if(t>k){
+            dem++;
+            for(int i=1; i<=k; i++){
+                System.out.print(a[i] +" ");
+            }
+            System.out.println();
+            return;
+        }
+        
+        for(int i= a[t-1] +1; i<=n-k+t; i++){
+            a[t]=i;
+            Try(t+1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n= sc.nextInt();
+        k=sc.nextInt();
+        a= new int[k+1];
+        a[0]=0;
+        Try(1);
+        System.out.println("Tong cong co " + dem+" to hop");
+    }
+}
